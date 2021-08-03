8e31e102ffb2b38820fdfcfc0c742a5756030d88
Marc Pages <marc.pages@live.com>
2021-08-03 17:01:54 +0200

    add read fasta, fast5, normalization and modelling

---
+++ src/normalization.py
@@ -0,0 +1,124 @@
+"""Functions for data normalization
+"""
+
+import numpy as np
+from scipy.signal import find_peaks
+
+## normalize the signal
+def med_mad(signal, factor=1.4826):
+    """
+    Calculate signal median and median absolute deviation
+    
+    Args:
+        signal (np.array): array of data to calculate med and mad
+        factor (float): factor to scale the mad
+        
+    Returns:
+        float, float : med and mad values
+    """
+    med = np.median(signal)
+    mad = np.median(np.absolute(signal - med)) * factor
+    return med, mad
+
+
+def find_noisiest_section(signal, samples=100, threshold=6.0):
+    """Find the noisiest section of a signal.
+    
+    Args:
+        signal (np.array): raw nanopore signal
+        samples (int): defaults to 100
+        threshold (float): defaults to 6.0
+        
+    Returns:
+        np.array : with a section (or all) the input signal that has the noisiest section
+    """
+    
+    threshold = signal.std() / threshold
+    noise = np.ones(signal.shape)
+
+    for idx in np.arange(signal.shape[0] // samples):
+        window = slice(idx * samples, (idx + 1) * samples)
+        noise[window] = np.where(signal[window].std() > threshold, 1, 0)
+
+    # start and end low for peak finding
+    noise[0] = 0; noise[-1] = 0
+    peaks, info = find_peaks(noise, width=(None, None))
+
+    if len(peaks):
+        widest = np.argmax(info['widths'])
+        tonorm = signal[info['left_bases'][widest]: info['right_bases'][widest]]
+    else:
+        tonorm = signal
+        
+    return tonorm
+
+def scale_signal(signal, offset, range, digitisation):
+    """Scale the signal to pA as explained in taiyaki
+    
+    Args:
+        signal (np.array): raw signal to be normalized
+        offset (int): offset as indicated in the attributes of the read fast5 file
+        range (float): range as indicated in the attributes of the read fast5 file
+        digitisation (float): as indicated in the attributes of the read fast5 file
+        
+    Returns:
+        np.array : scaled signal
+    """
+
+    scaled_signal = (signal + offset) * range/digitisation
+    
+    return scaled_signal
+
+def normalize_signal(signal, med, mad):
+    """ Rescale a signal based on the med and mad
+    
+    The signal is median centered and mad scaled.
+    
+    Args:
+        signal (np.array): signal to be rescaled
+        med (float): median to be substracted
+        mad (float): median absolute deviation to be used for scaling
+        
+    Returns:
+        np.array with the normalized signal
+    """
+    
+    signal = signal.astype(np.float32)
+    signal -= med
+    signal /= mad
+    return signal
+
+def normalize_signal_wrapper(signal, offset, range, digitisation, method = 'noisiest', samples=100, threshold=6.0, factor=1.4826):
+    """Wrapper function to normalize the nanopore raw signal
+    
+    Args:
+        signal (np.array): raw signal to be normalized
+        offset (int): offset as indicated in the attributes of the read fast5 file
+        range (float): range as indicated in the attributes of the read fast5 file
+        digitisation (float): as indicated in the attributes of the read fast5 file
+        method (str): how to define the range of values to use to calculate the
+            med and the mad. Can be "noisiest" (searches for the noisiest part
+            in the signal) or "all" (uses all the signal).
+        samples (int): used to find noisiest section, defaults to 100
+        threshold (float): used to find the noisiest section, defaults to 6.0
+        factor (float): mad scaler, defaults to 1.4826
+        
+    Returns:
+        np.array: normalized signal
+    """
+    
+    scaled_signal = scale_signal(signal, offset = offset, range = range, digitisation = digitisation)
+    
+    if method == 'noisiest':
+        med_mad_signal = find_noisiest_section(scaled_signal, 
+                                               samples=samples, 
+                                               threshold=threshold)
+    elif method == 'all':
+        med_mad_signal = scaled_signal
+    else:
+        raise ValueError('Method should be "noisiest" or "all"')
+    
+    med, mad = med_mad(med_mad_signal, factor = factor)
+    
+    normalized_signal = normalize_signal(scaled_signal, med = med, mad = mad)
+    return normalized_signal
---
+++ src/read.py
@@ -0,0 +1,169 @@
+"""
+Functions and classes to read data.
+"""
+
+import h5py
+import os
+import numpy as np
+from ont_fast5_api.fast5_interface import get_fast5_file
+
+class ReadData:
+
+    """Contains all the data for a Nanopore read
+    
+    Attributes:
+        read_id (str): id of the read
+        run_id (str): id of the run
+        filename (str): filename where the read is stored
+        offset (int): offset of the read signal
+        sampling_rate (float): sampling rate of the run
+        scaling (float): range/digitisation
+        range (float): range of the raw data
+        digitisation (float): digitisation
+        mux (int): ?
+        channel (str): pore channel
+        start (float): time when the read was started to sequence, in seconds
+        duration (float): time it took to sequence the read, in seconds
+        template_start (float): time when the read was started to sequence, in seconds
+        template_duration (float): time it took to sequence the read, in seconds
+        raw (np.array::int): raw signal
+        scaled (np.array::float): scaled signal
+        signal (np.array::float): normalized scaled signal
+        basecalls (str): basecalls of the read, optional
+        phredq (str): quality score of the basecalls, optional
+        segmentation (np.array::object): info regarding the segments of the read by tombo
+        start_rel_to_raw (int): datapoint where the aligned read starts
+        alignment_info (dict): info regarding the alignment that tombo made
+        
+    """
+    
+    def __init__(self, read, filename):
+
+        self.read_id = read.read_id
+        self.run_id = read.get_run_id().decode()
+        self.filename = os.path.basename(read.filename)
+
+        read_attrs = read.handle[read.raw_dataset_group_name].attrs
+        channel_info = read.handle[read.global_key + 'channel_id'].attrs
+
+        ## signal normalization stuff
+        self.offset = int(channel_info['offset'])
+        self.sampling_rate = channel_info['sampling_rate']
+        self.scaling = channel_info['range'] / channel_info['digitisation']
+        self.range = channel_info['range']
+        self.digitisation = channel_info['digitisation']
+
+        ## channel stuff
+        self.mux = read_attrs['start_mux']
+        self.channel = channel_info['channel_number'].decode()
+        self.start = read_attrs['start_time'] / self.sampling_rate
+        self.duration = read_attrs['duration'] / self.sampling_rate
+
+        # no trimming
+        self.template_start = self.start
+        self.template_duration = self.duration
+        
+        ## signal part
+        self.raw = read.handle[read.raw_dataset_name][:]
+        
+        ## if there are basecalls
+        try:
+            self.basecalls = read.get_analysis_dataset('Basecall_1D_000', 'BaseCalled_template/Fastq').split('\n')[1]
+            self.phredq = read.get_analysis_dataset('Basecall_1D_000', 'BaseCalled_template/Fastq').split('\n')[-2]
+        except:
+            self.basecalls = None
+            self.phredq = None
+        
+        ## if there is tombo resquiggle info
+        try:
+            ## this succeeds if the data is put by tombo
+            self.segmentation = read.get_analysis_dataset('RawGenomeCorrected_000', 'BaseCalled_template/Events')
+            self.start_rel_to_raw = read.get_analysis_attributes('RawGenomeCorrected_000/BaseCalled_template/Events')['read_start_rel_to_raw']
+            self.alignment_info = read.get_analysis_attributes('RawGenomeCorrected_000/BaseCalled_template/Alignment')
+        except TypeError:
+            try:
+                ## if the data we put it ourselves we use this
+                self.segmentation = read.handle['RawGenomeCorrected_000']['BaseCalled_template/Events'][:]
+                self.start_rel_to_raw = read.handle['RawGenomeCorrected_000']['BaseCalled_template/Events'].attrs['read_start_rel_to_raw']
+                self.alignment_info = None
+            except KeyError:
+                self.segmentation = None
+                self.start_rel_to_raw = None
+                self.alignment_info = None
+            
+    
+    def is_basecalled(self):
+        if self.basecalls is None:
+            return False
+        else:
+            return True
+    
+    def is_resquiggled(self):
+        if self.segmentation is None:
+            return False
+        else:
+            return True
+
+
+
+def list_reads_ids(fast5_file):
+    """Get the available read ids from a fast5 file
+
+    Args:
+    	fast5_file (str): fast5 file that we want the ids of
+
+    Returns:
+    	A list with the read ids in the file.
+    """
+    
+    return(get_fast5_file(fast5_file, 'r').get_read_ids())
+
+def handle_fast5_file(fast5_file):
+    """Open a fast5 file with the ONT API
+    """
+    
+    f5_handler = get_fast5_file(fast5_file, 'r')
+    
+    if len(f5_handler.get_read_ids()) < 1:
+        raise InputError('This file does not contain read ids')
+        
+    return f5_handler
+    
+def read_fast5(fast5_file, read_ids = None):
+    """Extract the data from a fast5 file from the desired reads
+
+    Args:
+    	fast5_file (str): file to be read
+
+    Returns:
+    	A dictionary with read ids as keys and ReadData objects as
+    		values.
+    """
+    
+    read_reads = dict()
+    with get_fast5_file(fast5_file, 'r') as f5_fh:
+        if read_ids is None:
+            for read in f5_fh.get_reads():
+                read_reads[read.read_id] = (ReadData(read, fast5_file))
+        else:
+            if not isinstance(read_ids, list):
+                read_ids = [read_ids]
+            for read_id in read_ids:
+                read = f5_fh.get_read(read_id)
+                read_reads[read.read_id] = (ReadData(read, fast5_file))
+        
+    return read_reads
+
+def read_fasta(fasta_file):
+    
+    if not fasta_file.endswith('.fasta'):
+        raise ValueError('File should be fasta')
+        
+    fasta_dict = dict()
+    with open(fasta_file, 'r') as handle:
+        for line in handle:
+            if line.startswith('>'):
+                k = line[1:].strip('\n')
+            else:
+                fasta_dict[k] = line.strip('\n')
+    return fasta_dict
