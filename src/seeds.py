cee56955342e69c1cbe2df494ea63023936177da
Marc Pages <marc.pages@live.com>
2021-08-13 16:46:41 +0200

    update seeds and constants

---
+++ src/seeds.py
@@ -0,0 +1,20 @@
+"""
+Here the seeds used for the random steps are defined to ensure reproducibility
+"""
+
+### INTER-SPECIES TASK 
+
+# species selection
+INTER_START_SEED = 1
+INTER_INNER_SEED = 1
+
+# read selection
+INTER_READ_SEED = 1
+
+## Chromosome regions for global
+
+GLOBAL_SEED = 1
+
+## Chromosomes for human
+
+HUMAN_CHROM_SEED = 1
