63fa77c879a26823a32ba9ca9e22cfbba5651924
Marc Pages <marc.pages@live.com>
2021-09-15 18:08:23 +0200

    finish dataset

--- src/classes.py
+++ src/classes.py
@@ -1,7 +1,8 @@
+import os
 from torch import nn
 from torch.utils.data import Dataset, Sampler
 from abc import abstractmethod
-import h5py
+import numpy as np
 import random
 
 class BaseModel(nn.Module):
@@ -24,6 +25,7 @@ class BaseModel(nn.Module):
         self.clipping_value = clipping_value
         self.use_sam = use_sam
         
+        self.to(self.device())
         self.init_weights()
     
     @abstractmethod    
@@ -33,7 +35,7 @@ class BaseModel(nn.Module):
             batch (dict): dict filled with tensors of input and output
         """
         raise NotImplementedError()
-        return predictions, losses
+        return losses, predictions
     
     @abstractmethod
     def validate_step(self, batch):
@@ -42,7 +44,7 @@ class BaseModel(nn.Module):
             batch (dict): dict filled with tensors of input and output
         """
         raise NotImplementedError()
-        return predictions, losses
+        return losses, predictions
     
     @abstractmethod    
     def predict(self):
@@ -68,19 +70,13 @@ class BaseModel(nn.Module):
         raise NotImplementedError()
         return loss, losses
     
-    @abstractmethod    
-    def optimize(self, y, p):
+    
+    def optimize(self, loss):
         """Optimizes the model by calculating the loss and doing backpropagation
         
         Args:
-            y (tensor): tensor with labels
-            p (tensor): tensor with predictions
-        Returns:
-            losses (dict): with detached values for each loss, the weighed sum is named
-                global_loss
+            loss (float): calculated loss that can be backpropagated
         """
-        
-        loss, losses = self.calculate_loss(y, p)
         
         if self.use_sam:
             loss.backward()
@@ -93,7 +89,7 @@ class BaseModel(nn.Module):
             torch.nn.utils.clip_grad_norm_(self.parameters(), self.clipping_value)
             optimizer.step()
             
-        return losses
+        return None
     
     def init_weights(self):
         """Initialize weights from uniform distribution
@@ -116,97 +112,177 @@ class BaseNanoporeDataset(Dataset):
     the raw data normalized. The second one contains an array of 
     byte-strings with the bases appended with ''.
     
+    This dataset already takes case of shuffling, for the dataloader set
+    shuffling to False.
+    
     Args:
-        hdf5_file (str): hdf5 file that has the data
+        data (str): dir with the npz files
         decoding_dict (dict): dictionary that maps integers to bases
         encoding_dict (dict): dictionary that maps bases to integers
         split (float): fraction of samples for training
         randomizer (bool): whether to randomize the samples order
         seed (int): seed for reproducible randomization
-        load_all (bool): whether to load all the data into memory
     """
 
-    def __init__(self, hdf5_file, decoding_dict, encoding_dict, 
-                 split = 0.95, shuffle = True, seed = None, load_all = False):
+    def __init__(self, data_dir, decoding_dict, encoding_dict, 
+                 split = 0.95, shuffle = True, seed = None):
         super(BaseNanoporeDataset, self).__init__()
         
-        self.hdf5_file = hdf5_file
+        self.data_dir = data_dir
         self.decoding_dict = decoding_dict
         self.encoding_dict = encoding_dict
         self.split = split
         self.shuffle = shuffle
         self.seed = seed
-        self.load_all = load_all
         
+        self.files_list = self._find_files()
+        self.num_samples_per_file = self._get_samples_per_file()
+        self.total_num_samples = np.sum(np.array(self.num_samples_per_file))
+        self.train_files_idxs = set()
+        self.validation_files_idxs = set()
         self.train_idxs = list()
         self.validation_idxs = list()
         self.train_sampler = None
         self.validation_sampler = None
-        self.split_train_validation()
+        self._split_train_validation()
+        self._get_samplers()
         
-        
-        if self.load_all:
-            self.data = self.load_data_into_memory()
-        else:
-            self.data = dict()
-
+        self.loaded_train_data = None
+        self.loaded_validation_data = None
+        self.current_loaded_train_idx = None
+        self.current_loaded_validation_idx = None
+    
     def __len__(self):
         """Number of samples
         """
-        with h5py.File(self.hdf5_file, "r") as datafile:
-            return datafile['x'].shape[0]
+        return self.total_num_samples
         
+    
     def __getitem__(self, idx):
         """Get a set of samples by idx
         
+        If the datafile is not loaded it loads it, otherwise
+        it uses the already in memory data.
+        
         Returns a dictionary
         """
-        if self.load_all:
-            return self.getitem_memory(idx)
+        if idx[0] in self.train_files_idxs:
+            if idx[0] != self.current_loaded_train_idx:
+                self.loaded_train_data = self.load_file_into_memory(idx[0])
+                self.current_loaded_train_idx = idx[0]
+            return self.get_data(data_dict = self.loaded_train_data, idx = idx[1])
+        elif idx[0] in self.validation_files_idxs:
+            if idx[0] != self.current_loaded_validation_idx:
+                self.loaded_validation_data = self.load_file_into_memory(idx[0])
+                self.current_loaded_validation_idx = idx[0]
+            return self.get_data(data_dict = self.loaded_validation_data, idx = idx[1])
         else:
-            return self.getitem_disk(idx)
+            raise IndexError('Given index not in train or validation files indices: ' + str(idx[0]))
         
-    def load_data_into_memory(self):
-        """Loads all the data into memory
+    
+    def _find_files(self):
+        """Finds list of files to read
         """
-        with h5py.File(self.hdf5_file, "r") as datafile:
-            x = datafile['x'][:]
-            y = datafile['y'][:]
-            y = y.astype('U1')
+        l = list()
+        for f in os.listdir(self.data_dir):
+            if f.endswith('.npz'):
+                l.append(f)
+        l = sorted(l)
+        return l
+    
+    
+    def _get_samples_per_file(self):
+        """Gets the number of samples per file from the file name
+        """
+        l = list()
+        for f in self.files_list:
+            l.append(int(f.split('_')[-1].split('.')[0]))
+        return l
+    
+    def _split_train_validation(self):
+        """Splits datafiles and idx for train and validation according to split
+        """
+        
+        # split train and validation data based on files
+        num_train_files = int(len(self.files_list) * self.split)
+        num_validation_files = len(self.files_list) - num_train_files
+        
+        files_idxs = list(range(len(self.files_list)))
+        if self.shuffle:
+            if self.seed:
+                random.seed(self.seed)
+            random.shuffle(files_idxs)
+            
+        self.train_files_idxs = set(files_idxs[:num_train_files])
+        self.validation_files_idxs = set(files_idxs[num_train_files:])
+        
+        # shuffle indices within each file and make a list of indices (file_idx, sample_idx)
+        # as tuples that can be iterated by the sampler
+        for idx in self.train_files_idxs:
+            sample_idxs = list(range(self.num_samples_per_file[idx]))
+            if self.shuffle:
+                if self.seed:
+                    random.seed(self.seed)
+                random.shuffle(sample_idxs)
+            for i in sample_idxs:
+                self.train_idxs.append((idx, i))
+        
+        for idx in self.validation_files_idxs:
+            sample_idxs = list(range(self.num_samples_per_file[idx]))
+            if self.shuffle:
+                if self.seed:
+                    random.seed(self.seed)
+                random.shuffle(sample_idxs)
+            for i in sample_idxs:
+                self.validation_idxs.append((idx, i))
+                
+        return None
+    
+    
+    def _get_samplers(self):
+        """Add samplers
+        """
+        self.train_sampler = IdxSampler(self.train_idxs, data_source = self)
+        self.validation_sampler = IdxSampler(self.validation_idxs, data_source = self)
+        return None
+        
+        
+    def load_file_into_memory(self, idx):
+        """Loads a file into memory and processes it
+        """
+        arr = np.load(os.path.join(self.data_dir, self.files_list[idx]))
+        x = arr['x']
+        y = arr['y']
         return self.process({'x':x, 'y':y})
-        
-    def getitem_disk(self, idx):
-        """Loads a portion of the data from disk
+    
+    
+    def get_data(self, data_dict, idx):
+        """Slices the data for given indices
         """
-        with h5py.File(self.hdf5_file, "r") as datafile:
-            x = datafile['x']
-            y = datafile['y']
-            return {'x':x[idx], 'idx':idx}
-            #return self.process({'x':x[idx], 'y':y[idx]})
-        
-    def getitem_memory(self, idx):
-        """Loads a portion of the data from memory
-        """
-        return {'x':self.data['x'][idx], 'y':self.data['y'][idx]}
-        
+        return {'x': data_dict['x'][idx], 'y': data_dict['y'][idx]}
+    
+    
     def process(self, data_dict):
         """Processes the data into a ready for training format
         """
         
         y = data_dict['y']
-        y = y.astype('U1')
+        if y.dtype != 'U1':
+            y = y.astype('U1')
         y = self.encode(y)
         data_dict['y'] = y
         return data_dict
+    
     
     def encode(self, y_arr):
         """Encode the labels
         """
         
+        new_y = np.zeros(y_arr.shape, dtype=int)
         for k, v in self.encoding_dict.items():
-            y_arr[y_arr == k] = v
-        y_arr = y_arr.astype(int)
-        return y_arr
+            new_y[y_arr == k] = v
+        return new_y
+    
     
     def decode(self):
         """Decode the labels
@@ -216,25 +292,7 @@ class BaseNanoporeDataset(Dataset):
         for k, v in self.decoding_dict.items():
             y_arr[y_arr == k] = v
         return y_arr
-    
-    def split_train_validation(self):
-        """Splits train and validation idxs and generates samplers
-        """
         
-        num_samples = self.__len__()
-        num_train_samples = int(num_samples * self.split)
-        num_validation_samples = num_samples - num_train_samples
-        
-        samples_idxs = list(range(num_samples))
-        if self.shuffle:
-            if self.seed:
-                random.seed(self.seed)
-            random.shuffle(samples_idxs)
-        
-        self.train_idxs = samples_idxs[:num_train_samples]
-        self.validation_idxs = samples_idxs[num_train_samples:]
-        self.train_sampler = IdxSampler(idxs = self.train_idxs, data_source = self)
-        self.validation_sampler = IdxSampler(idxs = self.validation_idxs, data_source = self)
         
 class IdxSampler(Sampler):
     """Sampler class to not sample from all the samples
