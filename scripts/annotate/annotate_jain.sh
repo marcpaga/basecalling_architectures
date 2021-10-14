627c995bccee89655087aa8b05d0a593ad5d2d20
marcpaga <m.pagesgallego@umcutrecht.nl>
2021-10-14 17:20:07 +0200

    refactor annotate jain script

--- scripts/annotate/annotate_jain.sh
+++ scripts/annotate/annotate_jain.sh
@@ -3,62 +3,70 @@
 # Usage
 # bash annotate_jain.sh $JAIN_DIR $N_CORES
 
-echo "Processing FAB42828"
-echo "Untarring and joining fastq"
-find "${1}/FAB42828" -name '*.fastq.gz' -exec gzip -d '{}' \;
-find "${1}/FAB42828" -name '*.fastq' -exec mv {} "${1}/FAB42828/fastq" \;
-cat "${1}/FAB42828/fastq/"*".fastq" > "${1}/FAB42828/fastq/all_basecalls.fastq"
+main_dir=$1
+n_cores=$2
 
-echo "Spliting fast5"
-multi_to_single_fast5 -i "${1}/FAB42828/fast5" -s "${1}/FAB42828/fast5" --recursive -t $2
+array=($main_dir/*/) # end with / to list only dirs
 
-echo "Merging fast5 and fastq"
-tombo preprocess annotate_raw_with_fastqs --fast5-basedir "${1}/FAB42828/fast5" --fastq-filenames "${1}/FAB42828/fastq/all_basecalls.fastq" --overwrite --processes $2
+for dir in ${array[@]}; do
+    
+    fname=$(basename $dir) # get deepest folder name
+    dir=${dir%?}
+    # skip genomes and tmp folder as we do not need them
+    if [ "$fname" == "genomes" ]; then
+        continue
+    fi
+    if [ "$fname" == "tmp" ]; then
+        continue
+    fi
 
-echo "Resquiggleling"
-tombo resquiggle "${1}/FAB42828/fast5" "${1}/genomes/homo_sapiens.fasta" --processes $2 --dna --num-most-common-errors 5 --overwrite
+    echo "Processing files in ${dir}"
+    # untar all the fastq files and join them into a single fastq file
+   
+    if [ ! -f "${dir}/fastq/all_basecalls.fastq" ]
+    then
+        echo "  Decompressing fastq"
+        find "${dir}/fastq" -name '*.fastq.gz' -exec gzip -d '{}' \;
+        find "${dir}/fastq" -name '*.fastq' -exec mv {} "${dir}/fastq" \;
 
-echo 'Reporting results'
-python annotate_teng.py --fast5-path "${1}/FAB42828/fast5" --output-file "${1}/FAB42828/segmentation_report.txt" --n-cores $2 --report --verbose
+        echo "  Joining fastq"
+        cat "${dir}/fastq/"*".fastq" > "${dir}/fastq/all_basecalls.fasta"
+        rm "${dir}/fastq/"*".fastq"
+        rm -r "${dir}/fastq/Norwich"
+        rm -r "${dir}/fastq/Bham"
+        mv "${dir}/fastq/all_basecalls.fasta" "${dir}/fastq/all_basecalls.fastq"
+    fi
 
-echo "Processing FAF04090"
-echo "Untarring and joining fastq"
-find "${1}/FAF04090" -name '*.fastq.gz' -exec gzip -d '{}' \;
-find "${1}/FAF04090" -name '*.fastq' -exec mv {} "${1}/FAF04090/fastq" \;
-cat "${1}/FAF04090/fastq/"*".fastq" > "${1}/FAF04090/fastq/all_basecalls.fasta"
-mv "${1}/FAF04090/fastq/all_basecalls.fasta" "${1}/FAF04090/fastq/all_basecalls.fastq"
+    if [ ! -f "${dir}/segmentation_report.txt" ]
+    then
 
-echo "Spliting fast5"
-multi_to_single_fast5 -i "${1}/FAF04090/fast5" -s "${1}/FAF04090/fast5" --recursive -t $2
+        if [ ! -f "${dir}/fast5/filename_mapping.txt" ]
+        then
+            echo "  Spliting fast5"
+            multi_to_single_fast5 -i "${dir}/fast5" -s "${dir}/fast5" --recursive -t $2
+        fi
 
-echo "Merging fast5 and fastq"
-tombo preprocess annotate_raw_with_fastqs --fast5-basedir "${1}/FAF04090/fast5" --fastq-filenames "${1}/FAF04090/fastq/all_basecalls.fastq" --overwrite --processes $2
+        echo "  Deleting multifast5 files"
+        rm -r "${dir}/fast5/Norwich"
+        rm -r "${dir}/fast5/Bham"
 
-echo "Resquiggleling"
-tombo resquiggle "${1}/FAF04090/fast5" "${1}/genomes/homo_sapiens.fasta" --processes $2 --dna --num-most-common-errors 5 --overwrite
+        if [ ! -f "${dir}/fast5/annotated.txt" ]
+        then
+            echo "  Merging fast5 and fastq"
+            tombo preprocess annotate_raw_with_fastqs --fast5-basedir "${dir}/fast5" --fastq-filenames "${dir}/fastq/all_basecalls.fastq" --overwrite --processes $n_cores
+            echo "True" >> "${dir}/fast5/annotated.txt"
+        fi
 
-echo 'Reporting results'
-python annotate_teng.py --fast5-path "${1}/FAF04090/fast5" --output-file "${1}/FAF04090/segmentation_report.txt" --n-cores $2 --report --verbose
+        if [ ! -f "${dir}/fast5/resquiggled.txt" ]
+        then
+            echo "  Resquiggleling"
+            tombo resquiggle "${dir}/fast5" "${main_dir}/genomes/Homo_sapiens.fna" --processes $n_cores --dna --num-most-common-errors 5 --ignore-read-locks
+            echo "True" >> "${dir}/fast5/resquiggled.txt"
+        fi
 
-
-echo "Processing FAF09968"
-echo "Untarring and joining fastq"
-find "${1}/FAF09968" -name '*.fastq.gz' -exec gzip -d '{}' \;
-find "${1}/FAF09968" -name '*.fastq' -exec mv {} "${1}/FAF09968/fastq" \;
-cat "${1}/FAF09968/fastq/"*".fastq" > "${1}/FAF09968/fastq/all_basecalls.fasta"
-mv "${1}/FAF09968/fastq/all_basecalls.fasta" "${1}/FAF09968/fastq/all_basecalls.fastq"
-
-echo "Spliting fast5"
-multi_to_single_fast5 -i "${1}/FAF09968/fast5" -s "${1}/FAF09968/fast5" --recursive -t $2
-
-echo "Merging fast5 and fastq"
-tombo preprocess annotate_raw_with_fastqs --fast5-basedir "${1}/FAF09968/fast5" --fastq-filenames "${1}/FAF09968/fastq/all_basecalls.fastq" --overwrite --processes $2
-
-echo "Resquiggleling"
-tombo resquiggle "${1}/FAF09968/fast5" "${1}/genomes/homo_sapiens.fasta" --processes $2 --dna --num-most-common-errors 5 --overwrite
-
-echo 'Reporting results'
-python annotate_teng.py --fast5-path "${1}/FAF09968/fast5" --output-file "${1}/FAF09968/segmentation_report.txt" --n-cores $2 --report --verbose
+        echo '  Reporting results'
+        python annotate_jain.py --fast5-path "${dir}/fast5" --output-file "${dir}/segmentation_report.txt" --n-cores $n_cores --report --verbose
+    fi
 
 
-
+done
