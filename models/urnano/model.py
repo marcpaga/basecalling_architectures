3658846c0071012994b04b53788597a3d22cb881
marcpaga <m.pagesgallego@umcutrecht.nl>
2021-10-19 16:59:21 +0200

    add urnano model

---
+++ models/urnano/model.py
@@ -0,0 +1,90 @@
+"""Implementation of the SkeletonModel
+
+Based on: 
+https://github.com/nanoporetech/bonito
+"""
+
+import os
+import sys
+from torch import nn
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
+from classes import BaseModelImpl
+from layers import BonitoLinearCRFDecoder, URNetDownBlock, URNetFlatBlock, URNetUpBlock, URNet
+from constants import CRF_STATE_LEN, CRF_BIAS, CRF_SCALE, CRF_BLANK_SCORE , CRF_N_BASE 
+
+
+class URNanoModel(BaseModelImpl):
+    """URNano model
+
+    Based on: https://github.com/yaozhong/URnano/blob/master/models/model_unet.py
+    """
+    def __init__(self, convolution = None, rnn = None, decoder = None, *args, **kwargs):
+        super(URNanoModel, self).__init__(*args, **kwargs)
+        """
+        Args:
+            convolution (nn.Module): module with: in [batch, channel, len]; out [batch, channel, len]
+            rnn (nn.Module): module with: in [len, batch, channel]; out [len, batch, channel]
+            decoder (nn.Module): module with: in [len, batch, channel]; out [len, batch, channel]
+        """
+    
+        self.convolution = convolution
+        self.rnn = rnn
+        self.decoder = decoder
+        
+        if self.convolution is None or self.self.rnn is None or self.decoder is None:
+            self.load_default_configuration()
+
+    def forward(self, x):
+        """Forward pass of a batch
+        
+        Args:
+            x (tensor) : [batch, channels (1), len]
+        """
+        
+        x = self.convolution(x)
+        x = x.permute(2, 0, 1) # [len, batch, channels]
+        x, _ = self.rnn(x)
+        x = self.decoder(x)
+        
+        return x
+            
+        
+    def load_default_configuration(self, default_all = False):
+        """Sets the default configuration for one or more
+        modules of the network
+
+        Based on: https://github.com/yaozhong/URnano/blob/master/experiment/model/Unet.model.parameters.json
+        """
+        
+        padding = 'same'
+        stride = 1
+        n_channels = [64, 128, 256, 512]
+        kernel = 11
+        maxpooling = [2, 2, 2] # in the github json it is [3, 2, 2], changed because we use even number segments
+
+
+        if self.convolution is None or default_all:
+            down = nn.ModuleList([URNetDownBlock(1, n_channels[0], kernel, maxpooling[0], stride, padding),
+                                 URNetDownBlock(n_channels[0], n_channels[1], 3, maxpooling[1], stride, padding),
+                                 URNetDownBlock(n_channels[1], n_channels[2], 3, maxpooling[2], stride, padding)])
+            flat = nn.ModuleList([URNetFlatBlock(n_channels[2], n_channels[3], 3, stride, padding)])
+            up = nn.ModuleList([URNetUpBlock(n_channels[3], n_channels[2], 3, maxpooling[2], maxpooling[2], stride, padding), 
+                               URNetUpBlock(n_channels[2], n_channels[1], 3, maxpooling[1], maxpooling[1], stride, padding),
+                               URNetUpBlock(n_channels[1], n_channels[0], 3, maxpooling[0], maxpooling[0], stride, padding)])
+            
+            self.convolution = URNet(down, flat, up)
+        if self.rnn is None or default_all:
+            self.rnn = nn.GRU(n_channels[0], hidden_size = n_channels[0], num_layers = 3, bidirectional = True)
+        if self.decoder is None or default_all:
+            if self.model_type == 'ctc':
+                self.decoder = nn.Sequential(nn.Linear(int(n_channels[0]*2), 5), nn.LogSoftmax(-1))
+            elif self.model_type == 'crf':
+                self.decoder = BonitoLinearCRFDecoder(
+                    insize = n_channels[0], 
+                    n_base = CRF_N_BASE, 
+                    state_len = CRF_STATE_LEN, 
+                    bias=CRF_BIAS, 
+                    scale= CRF_SCALE, 
+                    blank_score= CRF_BLANK_SCORE
+                )
--- src/layers.py
+++ src/layers.py
@@ -310,8 +310,8 @@ class CausalCallConvBlock(nn.Module):
 
         # divide input channel for the second conv2 and the conv_res by 2
         # because the glu operation divides the number of channels in two
-        self.conv1 = CausalConv1d(self.first_channel, self.num_channels, self.kernel_size, 1, self.dilation)
-        self.conv2 = CausalConv1d(int(self.num_channels/2), self.num_channels, self.kernel_size, 1, self.dilation)
+        self.conv1 = weight_norm(CausalConv1d(self.first_channel, self.num_channels, self.kernel_size, 1, self.dilation))
+        self.conv2 = weight_norm(CausalConv1d(int(self.num_channels/2), self.num_channels, self.kernel_size, 1, self.dilation))
         self.conv_res = nn.Conv1d(self.first_channel, int(self.num_channels/2), 1, 1)
 
     def forward(self, x):
@@ -332,3 +332,146 @@ class CausalCallConvBlock(nn.Module):
 
         xb = F.relu(xb)
         return xb
+
+
+class URNetDownBlock(nn.Module):
+
+    def __init__(self, input_size, channel_size, conv_kernel, max_kernel, stride = 1, padding = 'same'):
+        super(URNetDownBlock, self).__init__()
+
+        self.conv1 = nn.Conv1d(input_size, channel_size, conv_kernel, stride, padding)
+        self.conv2 = nn.Conv1d(channel_size, channel_size, conv_kernel, stride, padding)
+        self.batchnorm1 = nn.BatchNorm1d(channel_size)
+        self.batchnorm2 = nn.BatchNorm1d(channel_size)
+        self.rnn = nn.GRU(channel_size, channel_size)
+        self.maxpool = nn.MaxPool1d(max_kernel)
+
+    def forward(self, x):
+        """Args:
+            x (tensor): shape [batch, channels, len]
+
+        Returns:
+            cnv_out (tensor): shape [batch, channels, len]
+            rrn_out (tensor): shape [len, batch, channels]
+            max_out (tensor): shape [batch, channels, len]
+        """
+       
+        x = self.conv1(x)
+        x = self.batchnorm1(x)
+        x = F.relu(x)
+
+        x = self.conv2(x)
+        x = self.batchnorm2(x)
+        cnv_out = F.relu(x)
+
+        x = cnv_out.permute(2, 0, 1)
+        rnn_out, _ = self.rnn(x)
+        
+        rnn_out = rnn_out.permute(1, 2, 0)
+        max_out = self.maxpool(rnn_out)
+
+        return cnv_out, rnn_out, max_out
+
+class URNetFlatBlock(nn.Module):
+
+    def __init__(self, input_size, channel_size, conv_kernel, stride = 1, padding = 'same'):
+        super(URNetFlatBlock, self).__init__()
+
+        self.conv1 = nn.Conv1d(input_size, channel_size, conv_kernel, stride, padding)
+        self.conv2 = nn.Conv1d(channel_size, channel_size, conv_kernel, stride, padding)
+        self.batchnorm1 = nn.BatchNorm1d(channel_size)
+        self.batchnorm2 = nn.BatchNorm1d(channel_size)
+
+    def forward(self, x):
+        """Args:
+            x (tensor): shape [batch, channels, len]
+
+        Returns:
+            x (tensor): shape [batch, channels, len]
+        """
+
+        x = self.conv1(x)
+        x = self.batchnorm1(x)
+        x = F.relu(x)
+
+        x = self.conv2(x)
+        x = self.batchnorm2(x)
+        x = F.relu(x)
+
+        return x
+
+class URNetUpBlock(nn.Module):
+
+    def __init__(self, input_size, channel_size, conv_kernel, up_kernel, up_stride, conv_stride = 1, padding = 'same'):
+        super(URNetUpBlock, self).__init__()
+
+        self.conv1 = nn.Conv1d(int(channel_size*3), channel_size, conv_kernel, conv_stride, padding)
+        self.conv2 = nn.Conv1d(channel_size, channel_size, conv_kernel, conv_stride, padding)
+        self.batchnorm1 = nn.BatchNorm1d(channel_size)
+        self.batchnorm2 = nn.BatchNorm1d(channel_size)
+        self.upconv = nn.ConvTranspose1d(input_size, channel_size, up_kernel, up_stride)
+
+    def forward(self, x, cnn_in, rnn_in):
+        """Args:
+            x (tensor): shape [batch, channels, len]
+            cnn_in (tensor): shape [batch, channels, len]
+            rnn_in (tensor): shape [batch, channels, len]
+
+        Returns:
+            out (tensor): shape [batch, channels, len]
+        """
+
+        x = self.upconv(x)
+        x = torch.cat([x, cnn_in, rnn_in], dim = 1) # concatenate on channel dim
+        x = self.conv1(x)
+        x = self.batchnorm1(x)
+        x = F.relu(x)
+
+        x = self.conv2(x)
+        x = self.batchnorm2(x)
+        out = F.relu(x)
+
+        return out
+
+class URNet(nn.Module):
+
+    def __init__(self, down, flat, up):
+        super(URNet, self).__init__()
+        """Args:
+            down (nn.ModuleList): list of URNetDownBlock(s) to be used
+            flat (nn.ModuleList): list of URNetFlatBlock(s) to be used
+            up   (nn.ModuleList): list of URNetUpBlock(s) to be used
+        """
+
+        self.down = down
+        self.flat = flat
+        self.up = up
+
+
+    def forward(self, x):
+        """Args:
+            x (tensor): shape [batch, channels, len]
+            cnn_in (tensor): shape [batch, channels, len]
+            rnn_in (tensor): shape [batch, channels, len]
+
+        Returns:
+            out (tensor): shape [batch, channels, len]
+        """
+
+        cnv_list = list()
+        rnn_list = list()
+        for layer in self.down:
+            cnv_out, rnn_out, x =layer(x)
+            cnv_list.append(cnv_out)
+            rnn_list.append(rnn_out)
+
+
+        cnv_list = cnv_list[::-1]
+        rnn_list = rnn_list[::-1]
+        for layer in self.flat:
+            x = layer(x)
+
+        for i, layer in enumerate(self.up):
+            x = layer(x, cnv_list[i], rnn_list[i])
+
+        return x
