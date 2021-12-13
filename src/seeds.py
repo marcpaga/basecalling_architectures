43fd229c8ef60621d80c87704cbcefcce93470ae
marcpaga <m.pagesgallego@umcutrecht.nl>
2021-12-13 16:28:00 +0100

    add number of reads per task

--- src/seeds.py
+++ src/seeds.py
@@ -1,5 +1,6 @@
 """
 Here the seeds used for the random steps are defined to ensure reproducibility
+And also the amount of reads for each task
 """
 
 ### INTER-SPECIES TASK 
@@ -11,6 +12,13 @@ INTER_INNER_SEED = 1
 # read selection
 INTER_READ_SEED = 1
 
+# number of reads 
+MAX_SPECIES = 12
+MIN_SPECIES = 10
+INTER_TRAIN = 50000
+INTER_TRAIN_TEST = 5000
+INTER_TEST = 20000
+
 ### GLOBAL TASK
 
 # reads selection
@@ -19,9 +27,21 @@ GLOBAL_SEED = 1
 # odd chromosomes for training in human
 GLOBAL_TRAIN_ODD = 1
 
+# number of reads
+GLOBAL_TRAIN = 100000
+GLOBAL_TEST = 50000
+
 ### HUMAN TASK
 HUMAN_SEED = 1
 HUMAN_TRAIN_ODD = 1
 
+# number of reads
+HUMAN_TRAIN = 50000
+HUMAN_TEST = 25000
+
 ### DATA PREPARE 
 DATA_PREPARE_READ_SHUFFLE = 1
+GENOME_SPLIT = 0.5
+
+
+
