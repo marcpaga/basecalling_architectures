e27527556471a8053540daf0ae3c34450b749ebe
marcpaga <m.pagesgallego@umcutrecht.nl>
2022-01-07 16:04:17 +0100

    reformat bonito

--- models/bonito/model.py
+++ models/bonito/model.py
@@ -10,27 +10,28 @@ from torch import nn
 
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
 from classes import BaseModelImpl
-from layers.bonito import BonitoLSTM, BonitoLinearCRFDecoder
-from constants import CRF_STATE_LEN, CRF_BIAS, CRF_SCALE, CRF_BLANK_SCORE , CRF_N_BASE 
+from layers.bonito import BonitoLSTM
 
 
 class BonitoModel(BaseModelImpl):
-    """BonitoCTC Model
+    """Bonito Model
     """
-    def __init__(self, convolution = None, rnn = None, decoder = None, *args, **kwargs):
+    def __init__(self, convolution = None, encoder = None, decoder = None, reverse = True, load_default = False, *args, **kwargs):
         super(BonitoModel, self).__init__(*args, **kwargs)
         """
         Args:
             convolution (nn.Module): module with: in [batch, channel, len]; out [batch, channel, len]
-            rnn (nn.Module): module with: in [len, batch, channel]; out [len, batch, channel]
+            encoder (nn.Module): module with: in [len, batch, channel]; out [len, batch, channel]
             decoder (nn.Module): module with: in [len, batch, channel]; out [len, batch, channel]
+            reverse (bool): if the first rnn layer starts with reverse 
         """
     
         self.convolution = convolution
-        self.rnn = rnn
+        self.encoder = encoder
         self.decoder = decoder
+        self.reverse = reverse
         
-        if self.convolution is None or self.self.rnn is None or self.decoder is None:
+        if load_default:
             self.load_default_configuration()
 
     def forward(self, x):
@@ -42,10 +43,64 @@ class BonitoModel(BaseModelImpl):
         
         x = self.convolution(x)
         x = x.permute(2, 0, 1) # [len, batch, channels]
-        x = self.rnn(x)
+        x = self.encoder(x)
         x = self.decoder(x)
         return x
-            
+
+    def build_cnn(self):
+
+        cnn = nn.Sequential(
+            nn.Conv1d(
+                in_channels = 1, 
+                out_channels = 4, 
+                kernel_size = 5, 
+                stride= 1, 
+                padding=5//2, 
+                bias=True),
+            nn.SiLU(),
+            nn.Conv1d(
+                in_channels = 4, 
+                out_channels = 16, 
+                kernel_size = 5, 
+                stride= 1, 
+                padding=5//2, 
+                bias=True),
+            nn.SiLU(),
+            nn.Conv1d(
+                in_channels = 16, 
+                out_channels = 384, 
+                kernel_size = 19, 
+                stride= 5, 
+                padding=19//2, 
+                bias=True),
+            nn.SiLU()
+        )
+        return cnn
+
+    def build_encoder(self, reverse):
+
+        if reverse:
+            encoder = nn.Sequential(BonitoLSTM(384, 384, reverse = True),
+                                    BonitoLSTM(384, 384, reverse = False),
+                                    BonitoLSTM(384, 384, reverse = True),
+                                    BonitoLSTM(384, 384, reverse = False),
+                                    BonitoLSTM(384, 384, reverse = True))
+        else:
+            encoder = nn.Sequential(BonitoLSTM(384, 384, reverse = False),
+                                    BonitoLSTM(384, 384, reverse = True),
+                                    BonitoLSTM(384, 384, reverse = False),
+                                    BonitoLSTM(384, 384, reverse = True),
+                                    BonitoLSTM(384, 384, reverse = False))
+        return encoder    
+
+    def get_defaults(self):
+        defaults = {
+            'cnn_output_size': 384, 
+            'cnn_output_activation': 'silu',
+            'encoder_input_size': 384,
+            'encoder_output_size': 384
+        }
+        return defaults
         
     def load_default_configuration(self, default_all = False):
         """Sets the default configuration for one or more
@@ -53,30 +108,8 @@ class BonitoModel(BaseModelImpl):
         """
         
         if self.convolution is None or default_all:
-            self.convolution = nn.Sequential(nn.Conv1d(1, 4, 
-                                                       kernel_size = 5, stride= 1, padding=5//2, bias=True),
-                                             nn.SiLU(),
-                                             nn.Conv1d(4, 16, 
-                                                       kernel_size = 5, stride= 1, padding=5//2, bias=True),
-                                             nn.SiLU(),
-                                             nn.Conv1d(16, 384, 
-                                                       kernel_size = 19, stride= 5, padding=19//2, bias=True),
-                                             nn.SiLU())
+            self.convolution = self.build_cnn()
         if self.rnn is None or default_all:
-            self.rnn = nn.Sequential(BonitoLSTM(384, 384, reverse = True),
-                                     BonitoLSTM(384, 384, reverse = False),
-                                     BonitoLSTM(384, 384, reverse = True),
-                                     BonitoLSTM(384, 384, reverse = False),
-                                     BonitoLSTM(384, 384, reverse = True))
+            self.rnn = self.build_encoder(reverse = self.reverse)
         if self.decoder is None or default_all:
-            if self.model_type == 'ctc':
-                self.decoder = nn.Sequential(nn.Linear(384, 5), nn.LogSoftmax(-1))
-            elif self.model_type == 'crf':
-                self.decoder = BonitoLinearCRFDecoder(
-                    insize = 384, 
-                    n_base = CRF_N_BASE, 
-                    state_len = CRF_STATE_LEN, 
-                    bias=CRF_BIAS, 
-                    scale= CRF_SCALE, 
-                    blank_score= CRF_BLANK_SCORE
-                )
+            self.decoder = self.build_decoder(encoder_output_size = 384, model_type = self.model_type)
