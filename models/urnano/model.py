4172d1e29acb66d06101b4e20cc33e7e5c5e4bae
marcpaga <m.pagesgallego@umcutrecht.nl>
2022-01-07 17:27:09 +0100

    reformat urnano

--- models/urnano/model.py
+++ models/urnano/model.py
@@ -11,8 +11,6 @@ from torch import nn
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../src')))
 from classes import BaseModelImpl
 from layers.urnano import URNetDownBlock, URNetFlatBlock, URNetUpBlock, URNet
-from layers.bonito import BonitoLinearCRFDecoder
-from constants import CRF_STATE_LEN, CRF_BIAS, CRF_SCALE, CRF_BLANK_SCORE , CRF_N_BASE 
 
 
 class URNanoModel(BaseModelImpl):
@@ -20,7 +18,7 @@ class URNanoModel(BaseModelImpl):
 
     Based on: https://github.com/yaozhong/URnano/blob/master/models/model_unet.py
     """
-    def __init__(self, convolution = None, rnn = None, decoder = None, *args, **kwargs):
+    def __init__(self, convolution = None, rnn = None, decoder = None, load_default = False, *args, **kwargs):
         super(URNanoModel, self).__init__(*args, **kwargs)
         """
         Args:
@@ -33,7 +31,7 @@ class URNanoModel(BaseModelImpl):
         self.rnn = rnn
         self.decoder = decoder
         
-        if self.convolution is None or self.self.rnn is None or self.decoder is None:
+        if load_default:
             self.load_default_configuration()
 
     def forward(self, x):
@@ -50,6 +48,41 @@ class URNanoModel(BaseModelImpl):
         
         return x
             
+    def build_cnn(self):
+
+        padding = 'same'
+        stride = 1
+        n_channels = [64, 128, 256, 512]
+        kernel = 11
+        maxpooling = [2, 2, 2] # in the github json it is [3, 2, 2], changed because we use even number segments
+        
+        down = nn.ModuleList([URNetDownBlock(1, n_channels[0], kernel, maxpooling[0], stride, padding),
+                                 URNetDownBlock(n_channels[0], n_channels[1], 3, maxpooling[1], stride, padding),
+                                 URNetDownBlock(n_channels[1], n_channels[2], 3, maxpooling[2], stride, padding)])
+        flat = nn.ModuleList([URNetFlatBlock(n_channels[2], n_channels[3], 3, stride, padding)])
+        up = nn.ModuleList([URNetUpBlock(n_channels[3], n_channels[2], 3, maxpooling[2], maxpooling[2], stride, padding), 
+                            URNetUpBlock(n_channels[2], n_channels[1], 3, maxpooling[1], maxpooling[1], stride, padding),
+                            URNetUpBlock(n_channels[1], n_channels[0], 3, maxpooling[0], maxpooling[0], stride, padding)])
+        
+        cnn = nn.ModuleList([URNet(down, flat, up), 
+                            nn.Conv1d(n_channels[0], n_channels[0], 3, stride, padding), 
+                            nn.BatchNorm1d(n_channels[0]), 
+                            nn.ReLU()])
+        cnn = nn.Sequential(*cnn)
+        return cnn
+
+    def build_encoder(self):
+        encoder = nn.GRU(64, hidden_size = 64, num_layers = 3, bidirectional = True)
+        return encoder
+
+    def get_defaults(self):
+        defaults = {
+            'cnn_output_size': 64, 
+            'cnn_output_activation': 'relu',
+            'encoder_input_size': 64,
+            'encoder_output_size': 128
+        }
+        return defaults
         
     def load_default_configuration(self, default_all = False):
         """Sets the default configuration for one or more
@@ -57,39 +90,10 @@ class URNanoModel(BaseModelImpl):
 
         Based on: https://github.com/yaozhong/URnano/blob/master/experiment/model/Unet.model.parameters.json
         """
-        
-        padding = 'same'
-        stride = 1
-        n_channels = [64, 128, 256, 512]
-        kernel = 11
-        maxpooling = [2, 2, 2] # in the github json it is [3, 2, 2], changed because we use even number segments
 
-
-        if self.convolution is None or default_all:
-            down = nn.ModuleList([URNetDownBlock(1, n_channels[0], kernel, maxpooling[0], stride, padding),
-                                 URNetDownBlock(n_channels[0], n_channels[1], 3, maxpooling[1], stride, padding),
-                                 URNetDownBlock(n_channels[1], n_channels[2], 3, maxpooling[2], stride, padding)])
-            flat = nn.ModuleList([URNetFlatBlock(n_channels[2], n_channels[3], 3, stride, padding)])
-            up = nn.ModuleList([URNetUpBlock(n_channels[3], n_channels[2], 3, maxpooling[2], maxpooling[2], stride, padding), 
-                               URNetUpBlock(n_channels[2], n_channels[1], 3, maxpooling[1], maxpooling[1], stride, padding),
-                               URNetUpBlock(n_channels[1], n_channels[0], 3, maxpooling[0], maxpooling[0], stride, padding)])
-            
-            self.convolution = nn.ModuleList([URNet(down, flat, up), 
-                                              nn.Conv1d(n_channels[0], n_channels[0], 3, stride, padding), 
-                                              nn.BatchNorm1d(n_channels[0]), 
-                                              nn.ReLU()])
-
+        if self.convolution is None or default_all:          
+            self.convolution = self.build_cnn()
         if self.rnn is None or default_all:
-            self.rnn = nn.GRU(n_channels[0], hidden_size = n_channels[0], num_layers = 3, bidirectional = True)
+            self.rnn = self.build_encoder()
         if self.decoder is None or default_all:
-            if self.model_type == 'ctc':
-                self.decoder = nn.Sequential(nn.Linear(int(n_channels[0]*2), 5), nn.LogSoftmax(-1))
-            elif self.model_type == 'crf':
-                self.decoder = BonitoLinearCRFDecoder(
-                    insize = n_channels[0], 
-                    n_base = CRF_N_BASE, 
-                    state_len = CRF_STATE_LEN, 
-                    bias=CRF_BIAS, 
-                    scale= CRF_SCALE, 
-                    blank_score= CRF_BLANK_SCORE
-                )
+            self.decoder = self.build_decoder(encoder_output_size = 128, model_type = self.model_type)
