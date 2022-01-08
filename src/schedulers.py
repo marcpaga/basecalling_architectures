77a791b88acc9e8e1103b2be46ef9d92103ded7e
marcpaga <m.pagesgallego@umcutrecht.nl>
2022-01-08 14:14:32 +0100

    add warmup and remove old schedulers

--- src/schedulers.py
+++ src/schedulers.py
@@ -1,38 +1,70 @@
-from torch.optim.lr_scheduler import LambdaLR
-import numpy as np
+'''
+MIT License
 
-def cosine_decay_schedule(y0, y1):
-    """
-    Cosine Decay Scheduler
-    """
-    return lambda t: y1 + 0.5 * (y0 - y1) * (np.cos(t * np.pi) + 1.0)
+Copyright (c) 2019 Ildoo Kim
 
-def linear_schedule(y0, y1):
-    """
-    Linear Scheduler
-    """
-    return lambda t: y0 + (y1 - y0) * t
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
 
-def piecewise_schedule(knots, funcs):
-    """
-    Piecewise Scheduler
-    """
-    def f(t):
-        i = np.searchsorted(knots, t)
-        t0 = 0.0 if i == 0 else knots[i - 1]
-        t1 = 1.0 if i == len(knots) else knots[i]
-        return funcs[i]((t - t0) / (t1 - t0))
-    return f
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+from torch.optim.lr_scheduler import _LRScheduler
+from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 
-def func_scheduler(optimizer, func, total_steps, warmup_steps=None, warmup_ratio=0.1, start_step=0):
+class GradualWarmupScheduler(_LRScheduler):
+    """ Gradually warm-up(increasing) learning rate in optimizer.
+    Proposed in 'Accurate, Large Minibatch SGD: Training ImageNet in 1 Hour'.
+    Args:
+        optimizer (Optimizer): Wrapped optimizer.
+        multiplier: target learning rate = base lr * multiplier if multiplier > 1.0. if multiplier = 1.0, lr starts from 0 and ends up with the base_lr.
+        total_epoch: target learning rate is reached at total_epoch, gradually
+        after_scheduler: after target_epoch, use this scheduler(eg. ReduceLROnPlateau)
     """
-    Learning Rate Scheduler
-    """
-    if warmup_steps:
-        y0 = func(0.0)
-        func = piecewise_schedule(
-            [warmup_steps / total_steps],
-            [linear_schedule(warmup_ratio * y0, y0), func]
-        )
-    return LambdaLR(optimizer, (lambda step: func((step + start_step) / total_steps)))
+
+    def __init__(self, optimizer, multiplier, total_epoch, after_scheduler=None):
+        self.multiplier = multiplier
+        if self.multiplier < 1.:
+            raise ValueError('multiplier should be greater thant or equal to 1.')
+        self.total_epoch = total_epoch
+        self.after_scheduler = after_scheduler
+        self.finished = False
+        super(GradualWarmupScheduler, self).__init__(optimizer)
+
+    def get_lr(self):
+        if self.last_epoch > self.total_epoch:
+            if self.after_scheduler:
+                if not self.finished:
+                    self.after_scheduler.base_lrs = [base_lr * self.multiplier for base_lr in self.base_lrs]
+                    self.finished = True
+                return self.after_scheduler.get_last_lr()
+            return [base_lr * self.multiplier for base_lr in self.base_lrs]
+
+        if self.multiplier == 1.0:
+            return [base_lr * (float(self.last_epoch) / self.total_epoch) for base_lr in self.base_lrs]
+        else:
+            return [base_lr * ((self.multiplier - 1.) * self.last_epoch / self.total_epoch + 1.) for base_lr in self.base_lrs]
+
+    def step(self, epoch=None, metrics=None):
+        if self.finished and self.after_scheduler:
+            if epoch is None:
+                self.after_scheduler.step(None)
+            else:
+                self.after_scheduler.step(epoch - self.total_epoch)
+            self._last_lr = self.after_scheduler.get_last_lr()
+        else:
+            return super(GradualWarmupScheduler, self).step(epoch)
